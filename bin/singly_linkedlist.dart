234eab3bc3c8406176d9d81a028a48354f630c55
Syamkrishna2272 <you@example.com>
2024-02-26 18:42:42 +0530

    first commit

---
+++ bin/singly_linkedlist.dart
@@ -0,0 +1,34 @@
+main() {
+  addnodeNewvalue(5);
+  addnodeNewvalue(10);
+  addnodeNewvalue(15);
+  addnodeNewvalue(20);
+  display();
+}
+
+class node {
+  int? data;
+  node? next;
+  node(this.data);
+}
+
+node? head;
+node? tail;
+
+addnodeNewvalue(int data) {
+  node newnode = node(data);
+  if (head == null) {
+    head = newnode;
+  } else {
+    tail?.next = newnode;
+  }
+  tail = newnode;
+}
+
+display() {
+  node? temp = head;
+  while (temp != null) {
+    print(temp.data);
+    temp = temp.next;
+  }
+}
---
+++ test/dsa_learning_test.dart
@@ -0,0 +1,8 @@
+import 'package:dsa_learning/dsa_learning.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
