56d9abcc48290a72e9c736ce320c4b27bc692247
Syamkrishna2272 <syamkrishnashyam@gmail.com>
2024-03-06 10:02:19 +0530

    updated

--- bin/dsa_learning.dart
+++ bin/dsa_learning.dart
@@ -1,62 +1,14 @@
 main() {
-  add(10);
-  add(20);
-  add(30);
-  add(40);
-  add(50);
-  add(60);
-  add(70);
-  add(80);
-  add(90);
-  delete(70);
-  display();
+  List<int> a = [1, 2, 3, 4, 5, 6];
+  linear(a, 5);
 }
 
-class node {
-  int? data;
-  node? next;
-  node(this.data);
-}
-
-node? head;
-node? tail;
-
-add(int data) {
-  node newnode = node(data);
-  if (head == null) {
-    head = newnode;
-  } else {
-    tail?.next = newnode;
+linear(List<int> a, int target) {
+  for (int i = 0; i < a.length; i++) {
+    if (a[i] == target) {
+      print("element found index is $i");
+      return;
+    }
   }
-  tail = newnode;
-}
-
-display() {
-  node? temp = head;
-  while (temp != null) {
-    print(temp.data);
-    temp = temp.next;
-  }
-}
-
-delete(int data) {
-  node? temp = head;
-  node? prev;
-  if (temp != null && temp.data == data) {
-    head = temp.next;
-    return;
-  }
-  while (temp != null && temp.data != data) {
-    prev = temp;
-    temp = temp.next;
-  }
-  if (temp == null) {
-    return;
-  }
-  if (temp == tail) {
-    tail = temp;
-    tail?.next = null;
-  }
-
-  prev?.next = temp.next;
+  print("Not element");
 }
